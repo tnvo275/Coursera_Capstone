6465036a844a98adf76f0265141f1016c3bb1329
tranvo95 <tranngt.vo@gmail.com>
2019-07-21 23:23:48 -0500

    Update first_notebook.py

--- first_notebook.py
+++ first_notebook.py
@@ -1,5 +1,4 @@
-This notebook will be mainly used for the Capstone Project
-
+#This notebook will be mainly used for the Capstone Project
 import pandas as pd
 import numpy as np
 print('Hello Capstone Project Course!')
